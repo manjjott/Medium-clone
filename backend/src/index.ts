6ee773cda32f87ba8b4151ea881c9d5d4b34149b
Manjot <manjotsingh1901@gmail.com>
2025-02-07 21:29:19 -0800

    provided auth and blog routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,16 @@ import { Hono } from "hono";
 import userRouter from "./routes/user";
 import blogrouter from "./routes/blog";
 
-const app = new Hono<{}>()
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+  Variables: {
+    userId: string;
+  };
+}>();
+
 
 app.route("/api/v1/user", userRouter);
 app.route("api/v1/blog", blogrouter);
--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,17 +1,18 @@
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
+import { getPrisma } from "../prismaFucntion";
 
 const blogRouter = new Hono<{
   Bindings: {
-    JWT_SECRET: string;
     DATABASE_URL: string;
+    JWT_SECRET: string;
   };
   Variables: {
     userId: string;
   };
 }>();
 
-blogRouter.use("*", async (c, next) => {
+blogRouter.use("/*", async (c, next) => {
   const authHeader = c.req.header("Authorization");
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     c.status(401);
@@ -20,29 +21,83 @@ blogRouter.use("*", async (c, next) => {
   const token = authHeader.split(" ")[1];
 
   try {
-  const user = await verify(token, c.env.JWT_SECRET);
-  c.set("userId", user.userId);
-  await next();
+    const user = (await verify(token, c.env.JWT_SECRET)) as { userId: string };
+    c.set("userId", user.userId);
+    await next();
   } catch (e) {
     c.status(401);
-    return c.json({error: "Invalid Token"});
+    return c.json({ error: "Invalid Token" });
   }
 });
 
-blogRouter.post("/", (c) => {
-  return c.text("Create Blog Page");
+blogRouter.post("/", async (c) => {
+  const body = await c.req.json();
+  const userId = c.get("userId");
+  if (!userId) {
+    return c.json({ error: "User ID missing from request." }, 401);
+  }
+  const { title, content } = body;
+  if (!title || !content) {
+    return c.json({ error: "Title and content are required!" }, 400);
+  }
+  const prisma = getPrisma(c.env.DATABASE_URL);
+
+  try {
+    const blog = await prisma.blog.create({
+      data: {
+        title: title,
+        content: content,
+        authorId: userId,
+      },
+    });
+    return c.json({ blog: blog });
+  } catch (e) {
+    return c.json({ error: e });
+  }
 });
 
-blogRouter.put("/:id", (c) => {
-  return c.text("Update Blog Page");
+blogRouter.put("/", async (c) => {
+  const body = await c.req.json();
+  const { title, content, id } = await c.req.json();
+  if (!title || !content || !id) {
+    return c.json({ error: "Title and content are required!" }, 400);
+  }
+
+  const prisma = getPrisma(c.env.DATABASE_URL);
+
+  try {
+    const blog = await prisma.blog.update({
+      where: {
+        id: body.id,
+        authorId: c.get("userId"),
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+    return c.json({ blog: blog });
+  } catch (e) {
+    return c.json({ error: e });
+  }
 });
 
 blogRouter.delete("/:id", (c) => {
   return c.text("Delete Blog Page");
 });
 
-blogRouter.get("/:id", (c) => {
-  return c.text(`Get Blog Page ${c.req.param("id")}`);
+blogRouter.get("/:id", async (c) => {
+  const prisma = getPrisma(c.env.DATABASE_URL);
+  try {
+    const blog = await prisma.blog.findUnique({
+      where: {
+        id: c.req.param("id"),
+      },
+    });
+    return c.json({ blog: blog });
+  } catch (e) {
+    return c.json({ error: e });
+  }
 });
 
 blogRouter.get("/bulk", (c) => {
